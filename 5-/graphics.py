c7a11ede56bd0deb658b4633be9cc48822f4ece8
Berat Gumus <beratgumus9@gmail.com>
2018-03-28 00:55:37 +0300

    Matplotlib

--- 5-/graphics.py
+++ 5-/graphics.py
@@ -18,7 +18,36 @@ print(df_temp.Sex.value_counts())
 
 # to show up automatically without stopping execution, turn on interactive mode with plt.ion()
 plt.ion()
-df_temp.Sex.value_counts().plot(kind='bar')
+# df_temp.Sex.value_counts().plot(kind='bar')
 
-df.Age.value_counts().plot(kind='bar')
-#print(df.Age.hist(bins=10))
+# df.Age.value_counts().plot(kind='bar')
+# df.Age.hist(bins=10)
+# plt.show()
+
+aliveCustomers = df[df.Survived == 1]
+print(aliveCustomers)
+
+aliveCustomersGroupingByClass = df[df.Survived == 1].groupby('Pclass').count()
+print(aliveCustomersGroupingByClass)
+
+aliveCustomersGroupingByClass2 = df[df.Survived == 1].groupby('Pclass').Survived.value_counts()
+print(aliveCustomersGroupingByClass2)
+# df[df.Survived == 1].groupby('Pclass').Survived.value_counts().plot(kind='bar')
+
+aliveCustomersGroupingByClassAndAvearageCosts = df[df.Survived == 1].groupby('Pclass').Fare.mean()
+print(aliveCustomersGroupingByClassAndAvearageCosts)
+
+allCustomersGroupingByClassAndAvearageCosts = df.groupby('Pclass').Fare.mean()
+print(allCustomersGroupingByClassAndAvearageCosts)
+
+aliveCustomersGroupingByClassAndSexStats = df[df.Survived == 1].groupby('Pclass').Sex.value_counts()
+print(aliveCustomersGroupingByClassAndSexStats)
+# aliveCustomersGroupingByClassAndSexStats.plot(kind='bar')
+
+
+deadAndHasRelatives = df[(df.Survived == 0) & ((df.SibSp > 0) | (df.Parch > 0))]
+print(deadAndHasRelatives.head())
+
+deadAndHasRelativesGroupingByClass = deadAndHasRelatives.groupby('Pclass').Survived.count()
+print(deadAndHasRelativesGroupingByClass)
+#deadAndHasRelativesGroupingByClass.plot(kind='bar')
